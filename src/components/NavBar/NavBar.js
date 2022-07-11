a70a54e8dd6690e2cb74dc9ae6389ad508090832
Bailym <bm39@hotmail.co.uk>
2022-07-11 20:40:50 +0100

    add responsive nav

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,20 +1,66 @@
 import "./NavBar.css";
+import { useState, useEffect } from "react";
 
-function NavBar() {
+export function NavBar() {
     return (
         <div class="nav-div">
             <div class="nav-logo-div">
-                <a href="/"><img class="nav-logo" src="/NavLogo.png" alt="Logo"/></a>
+                <a href="/"><img class="nav-logo" src="/NavLogo.png" alt="Logo" /></a>
             </div>
             <div class="nav-items">
-                <ul class ="nav-items-list">
-                    <li class ="nav-item"><a href="/" class="underline">Item 1</a></li>
-                    <li class ="nav-item"><a href="/" class="underline">Item 2</a></li>
-                    <li class ="nav-item"><a href="/" class="underline">Item 3</a></li>
+                <ul class="nav-items-list">
+                    <li class="nav-item"><a href="/" class="underline">Item 1</a></li>
+                    <li class="nav-item"><a href="/" class="underline">Item 2</a></li>
+                    <li class="nav-item"><a href="/" class="underline">Item 3</a></li>
                 </ul>
             </div>
         </div>
     )
 }
 
-export default NavBar
+export function NavBarResponsive() {
+
+    const [openMenu, setOpenMenu] = useState(false);
+
+    //toggles the menu open or closed
+    function toggleMenu() {
+        setOpenMenu(openMenu => !openMenu);
+    }
+
+    if (openMenu) {
+        return (
+            <div class="nav-div-responsive-open">
+                <div class="nav-top-bar-responsive-open">
+                    <div class="nav-logo-div-open">
+                        <a href="/"><img class="nav-logo" src="/NavLogo.png" alt="Logo" /></a>
+                    </div>
+                    <div class="nav-hamburger-responsive-open">
+                        <img src="/Hamburger.png" alt="hamburger" onClick={() => toggleMenu()}></img>
+                    </div>
+                </div>
+                <div class="nav-items-responsive-open">
+                    <ul class="nav-items-list-responsive-open">
+                        <li class="nav-item-responsive-open"><a href="/" class="underline">Item 1</a></li>
+                        <li class="nav-item-responsive-open"><a href="/" class="underline">Item 2</a></li>
+                        <li class="nav-item-responsive-open"><a href="/" class="underline">Item 3</a></li>
+                    </ul>
+                </div>
+            </div>
+        )
+    }
+    else {
+        return (
+            <div class="nav-div-responsive-closed">
+                <div class="nav-logo-div-closed">
+                    <a href="/"><img class="nav-logo" src="/NavLogo.png" alt="Logo" /></a>
+                </div>
+                <div class="nav-hamburger-responsive-closed">
+                    <img src="/Hamburger.png" alt="hamburger" onClick={() => toggleMenu()}></img>
+                </div>
+            </div>
+        )
+    }
+}
+
+
+
--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,13 +1,29 @@
-import logo from '../../logo.svg';
-import Box from '@mui/material/Box';
-import NavBar from "../../components/NavBar/NavBar";
+import { NavBarResponsive, NavBar } from "../../components/NavBar/NavBar";
 import './App.css';
 import MediaWithText from '../../components/MediaWithContent/MediaWithContent';
+import { useState, useEffect } from "react";
 
 function App() {
+
+  const [isMobile, setMobile] = useState(window.innerWidth < 641);
+  const [isTablet, setTablet] = useState(window.innerWidth < 1007 && window.innerWidth > 640);
+  const [isDesktop, setDesktop] = useState(window.innerWidth > 1006);
+    
+    //work out what type of screen we are on
+    function updateMedia () {
+        setMobile(window.innerWidth < 600);
+    };
+
+    //listener to update screen size
+    useEffect(() => {
+        window.addEventListener("resize", updateMedia);
+        return () => window.removeEventListener("resize", updateMedia);
+      });
+
+
   return (
     <div class="app-container-div">
-      <NavBar/>
+      {isMobile ? <NavBarResponsive /> : <NavBar />}
       <MediaWithText/>
     </div>
     
