fc14679d1b5987bdbfe9cf1a20c2ca10d74c0df1
Bailym <bm39@hotmail.co.uk>
2022-07-14 10:43:27 +0100

    use env var for img links

--- src/components/MediaWithContent/MediaWithContent.js
+++ src/components/MediaWithContent/MediaWithContent.js
@@ -10,9 +10,9 @@ function MediaWithContent() {
             <motion.div className="content-div" animate={{x: ["100vw","0vw"]}}>
                 <div className="socials-div">
                     <ul className ="socials-list">
-                        <li className="socials-item"><a href="https://github.com/Bailym" target="_blank" rel="noreferrer" ><motion.img src="/Github.png" alt="github" id="github-img" whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}/></a></li>
-                        <li className="socials-item"><a href="https://www.linkedin.com/in/baily-martin-16b655152/" target="_blank" rel="noreferrer"><motion.img src="/LinkedIn.png" alt="linkedin" id="linkedin-img" whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}/></a></li>
-                        <li className="socials-item"><a href="https://www.instagram.com/bailym18/" target="_blank" rel="noreferrer"><motion.img src="/Instagram.png" alt="instagram" id="instagram-img" whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}/></a></li>
+                        <li className="socials-item"><a href="https://github.com/Bailym" target="_blank" rel="noreferrer" ><motion.img src={process.env.PUBLIC_URL + "/Github.png"} alt="github" id="github-img" whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}/></a></li>
+                        <li className="socials-item"><a href="https://www.linkedin.com/in/baily-martin-16b655152/" target="_blank" rel="noreferrer"><motion.img src={process.env.PUBLIC_URL + "/LinkedIn.png"} alt="linkedin" id="linkedin-img" whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}/></a></li>
+                        <li className="socials-item"><a href="https://www.instagram.com/bailym18/" target="_blank" rel="noreferrer"><motion.img src={process.env.PUBLIC_URL + "/Instagram.png"} alt="instagram" id="instagram-img" whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}/></a></li>
                     </ul>
                     <div id="cv-buttons">
                         <a href="/CV.pdf" target="_blank" rel="noreferrer"><motion.button whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>Download CV</motion.button></a>
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -3,15 +3,15 @@ import { useState, useEffect } from "react";
 
 export function NavBar() {
     return (
-        <div class="nav-div">
-            <div class="nav-logo-div">
-                <a href="/"><img class="nav-logo" src="/NavLogo.png" alt="Logo" /></a>
+        <div className="nav-div">
+            <div className="nav-logo-div">
+                <a href="/"><img className="nav-logo" src={process.env.PUBLIC_URL + "/NavLogo.png"} alt="Logo" /></a>
             </div>
-            <div class="nav-items">
-                <ul class="nav-items-list">
-                    <li class="nav-item"><a href="/" class="underline">Item 1</a></li>
-                    <li class="nav-item"><a href="/" class="underline">Item 2</a></li>
-                    <li class="nav-item"><a href="/" class="underline">Item 3</a></li>
+            <div className="nav-items">
+                <ul className="nav-items-list">
+                    <li className="nav-item"><a href="/" className="underline">Item 1</a></li>
+                    <li className="nav-item"><a href="/" className="underline">Item 2</a></li>
+                    <li className="nav-item"><a href="/" className="underline">Item 3</a></li>
                 </ul>
             </div>
         </div>
@@ -29,20 +29,20 @@ export function NavBarResponsive() {
 
     if (openMenu) {
         return (
-            <div class="nav-div-responsive-open">
-                <div class="nav-top-bar-responsive-open">
-                    <div class="nav-logo-div-open">
-                        <a href="/"><img class="nav-logo" src="/NavLogo.png" alt="Logo" /></a>
+            <div className="nav-div-responsive-open">
+                <div className="nav-top-bar-responsive-open">
+                    <div className="nav-logo-div-open">
+                        <a href="/"><img className="nav-logo" src={process.env.PUBLIC_URL + "/NavLogo.png"} alt="Logo" /></a>
                     </div>
-                    <div class="nav-hamburger-responsive-open">
-                        <img src="/Hamburger.png" alt="hamburger" onClick={() => toggleMenu()}></img>
+                    <div className="nav-hamburger-responsive-open">
+                        <img src={process.env.PUBLIC_URL + "/Hamburger.png"} alt="hamburger" onClick={() => toggleMenu()}></img>
                     </div>
                 </div>
-                <div class="nav-items-responsive-open">
-                    <ul class="nav-items-list-responsive-open">
-                        <li class="nav-item-responsive-open"><a href="/" class="underline">Item 1</a></li>
-                        <li class="nav-item-responsive-open"><a href="/" class="underline">Item 2</a></li>
-                        <li class="nav-item-responsive-open"><a href="/" class="underline">Item 3</a></li>
+                <div className="nav-items-responsive-open">
+                    <ul className="nav-items-list-responsive-open">
+                        <li className="nav-item-responsive-open"><a href="/" className="underline">Item 1</a></li>
+                        <li className="nav-item-responsive-open"><a href="/" className="underline">Item 2</a></li>
+                        <li className="nav-item-responsive-open"><a href="/" className="underline">Item 3</a></li>
                     </ul>
                 </div>
             </div>
@@ -50,12 +50,12 @@ export function NavBarResponsive() {
     }
     else {
         return (
-            <div class="nav-div-responsive-closed">
-                <div class="nav-logo-div-closed">
-                    <a href="/"><img class="nav-logo" src="/NavLogo.png" alt="Logo" /></a>
+            <div className="nav-div-responsive-closed">
+                <div className="nav-logo-div-closed">
+                    <a href="/"><img className={process.env.PUBLIC_URL + "nav-logo"} src="/NavLogo.png" alt="Logo" /></a>
                 </div>
-                <div class="nav-hamburger-responsive-closed">
-                    <img src="/Hamburger.png" alt="hamburger" onClick={() => toggleMenu()}></img>
+                <div className="nav-hamburger-responsive-closed">
+                    <img src={process.env.PUBLIC_URL + "/Hamburger.png"} alt="hamburger" onClick={() => toggleMenu()}></img>
                 </div>
             </div>
         )
