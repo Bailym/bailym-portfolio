36f1c8f008ed91f19e040946964a3d10b51f5d05
Baily Martin <BM39@hotmail.co.uk>
2023-07-18 19:07:23 +0100

    add elixirmix and reorder tiles

--- src/components/ProjectTiles/ProjectTiles.js
+++ src/components/ProjectTiles/ProjectTiles.js
@@ -7,8 +7,30 @@ function ProjectTiles() {
             <div className='tiles-title'>
                 <h1>Projects</h1>
             </div>
+
             <div className="tiles-container">
                 <motion.div className="tile" id="tile-1" whileHover={{ scale: 1.05 }}>
+                    <div className="tile-content">
+                        <h2>Extra Armor (Java + Minecraft Forge API)</h2>
+                        <img src="https://i.imgur.com/N003tfh.gif" alt="tcg-db"></img>
+                    </div>
+                    <div className='tile-footer'>
+                        <a href="https://www.curseforge.com/minecraft/mc-mods/extra-armor" target="_blank" rel="noreferrer"><button>View on CurseForge</button></a>
+                    </div>
+                </motion.div>
+
+                <motion.div className="tile" id="tile-2" whileHover={{ scale: 1.05 }}>
+                    <div className="tile-content">
+                        <h2>ElixirMix (Unity 2D)</h2>
+                        <img src={"https://github.com/Bailym/Bluefruit-GameJam-2023/blob/main/images/Mixing.png?raw=true"} alt="elixir mix game"></img>
+                    </div>
+                    <div className='tile-footer'>
+                        <a href="https://github.com/Bailym/Bluefruit-GameJam-2023" target="_blank" rel="noreferrer"><button>View on Github</button></a>
+                        <a href="https://github.com/Bailym/Bluefruit-GameJam-2023/releases/tag/v0.0.0" target="_blank" rel="noreferrer"><button>Download</button></a>
+                    </div>
+                </motion.div>
+
+                <motion.div className="tile" id="tile-3" whileHover={{ scale: 1.05 }}>
                     <div className="tile-content">
                         <h2>Binary Bingo (React.js)</h2>
                         <img src="https://github.com/Bailym/BinaryBingo/raw/main/images/BinaryBingo%205.png" alt="Binary Bingo"></img>
@@ -20,34 +42,29 @@ function ProjectTiles() {
 
                     </div>
                 </motion.div>
-                <motion.div className="tile" id="tile-5" whileHover={{ scale: 1.05 }}>
+
+                <motion.div className="tile" id="tile-3" whileHover={{ scale: 1.05 }}>
                     <div className="tile-content">
-                        <h2>Plant Care App (React.js + Node.js + MySQL)</h2>
-                        <img src="https://github.com/Bailym/PlantCareApp/blob/master/images/Poster.jpg?raw=true" alt="PlantCareApp"></img>
+                        <h2>Poll Tracker (React.js + Node.js + MongoDB)</h2>
+                        <img src="https://github.com/Bailym/PollTracker/blob/main/images/Home.png?raw=true" alt="Poll Tracker"></img>
                     </div>
                     <div className='tile-footer'>
-                        <a href="https://github.com/Bailym/PlantCareApp" target="_blank" rel="noreferrer"><button>View on Github</button></a>
-                        <a href="https://bailym-plantcareapp.herokuapp.com/" target="_blank" rel="noreferrer"><button>View Live</button></a>
+                        <a href="https://github.com/Bailym/PollTracker" target="_blank" rel="noreferrer"><button>View on Github</button></a>
+                        <a href="https://bailym-polltracker.herokuapp.com/" target="_blank" rel="noreferrer"><button>View Live</button></a>
                     </div>
                 </motion.div>
-                <motion.div className="tile" id="tile-4" whileHover={{ scale: 1.05 }}>
+
+                <motion.div className="tile" id="tile-3" whileHover={{ scale: 1.05 }}>
                     <div className="tile-content">
-                        <h2>Extra Armor (Java + Minecraft Forge API)</h2>
-                        <img src="https://i.imgur.com/N003tfh.gif" alt="tcg-db"></img>
+                        <h2>TCG-DB (React.js)</h2>
+                        <img src="https://github.com/Bailym/TCG-DB/blob/main/images/Base%20.png?raw=true" alt="tcg-db"></img>
                     </div>
                     <div className='tile-footer'>
-                        <a href="https://www.curseforge.com/minecraft/mc-mods/extra-armor" target="_blank" rel="noreferrer"><button>View on CurseForge</button></a>
+                        <a href="https://github.com/Bailym/TCG-DB" target="_blank" rel="noreferrer"><button>View on Github</button></a>
+                        <a href="https://bailym.github.io/TCG-DB/" target="_blank" rel="noreferrer"><button>View Live</button></a>
                     </div>
                 </motion.div>
-                <motion.div className="tile" id="tile-6" whileHover={{ scale: 1.05 }}>
-                    <div className="tile-content">
-                        <h2>FetchDecode.com (WordPress)</h2>
-                        <img src={process.env.PUBLIC_URL + "/fetchdecode.png"} alt="fetchdecode.com"></img>
-                    </div>
-                    <div className='tile-footer'>
-                        <a href="https://fetchdecode.com" target="_blank" rel="noreferrer"><button>View Live</button></a>
-                    </div>
-                </motion.div>
+
                 <motion.div className="tile" id="tile-2" whileHover={{ scale: 1.05 }}>
                     <div className="tile-content">
                         <h2>GalaxyRun (Unity 2D)</h2>
@@ -59,24 +76,15 @@ function ProjectTiles() {
 
                     </div>
                 </motion.div>
-                <motion.div className="tile" id="tile-3" whileHover={{ scale: 1.05 }}>
+
+                <motion.div className="tile" id="tile-5" whileHover={{ scale: 1.05 }}>
                     <div className="tile-content">
-                        <h2>TCG-DB (React.js)</h2>
-                        <img src="https://github.com/Bailym/TCG-DB/blob/main/images/Base%20.png?raw=true" alt="tcg-db"></img>
+                        <h2>Plant Care App (React.js + Node.js + MySQL)</h2>
+                        <img src="https://github.com/Bailym/PlantCareApp/blob/master/images/Poster.jpg?raw=true" alt="PlantCareApp"></img>
                     </div>
                     <div className='tile-footer'>
-                        <a href="https://github.com/Bailym/TCG-DB" target="_blank" rel="noreferrer"><button>View on Github</button></a>
-                        <a href="https://bailym.github.io/TCG-DB/" target="_blank" rel="noreferrer"><button>View Live</button></a>
-                    </div>
-                </motion.div>
-                <motion.div className="tile" id="tile-3" whileHover={{ scale: 1.05 }}>
-                    <div className="tile-content">
-                        <h2>Poll Tracker (React.js + Node.js + MongoDB)</h2>
-                        <img src="https://github.com/Bailym/PollTracker/blob/main/images/Home.png?raw=true" alt="Poll Tracker"></img>
-                    </div>
-                    <div className='tile-footer'>
-                        <a href="https://github.com/Bailym/PollTracker" target="_blank" rel="noreferrer"><button>View on Github</button></a>
-                        <a href="https://bailym-polltracker.herokuapp.com/" target="_blank" rel="noreferrer"><button>View Live</button></a>
+                        <a href="https://github.com/Bailym/PlantCareApp" target="_blank" rel="noreferrer"><button>View on Github</button></a>
+                        <a href="https://bailym-plantcareapp.herokuapp.com/" target="_blank" rel="noreferrer"><button>View Live</button></a>
                     </div>
                 </motion.div>
             </div>
