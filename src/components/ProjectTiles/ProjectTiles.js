dd1aa0ae9c45ee1b4c61c4ad0171c80b6f11b903
Baily Martin <BM39@hotmail.co.uk>
2024-01-06 13:13:30 +0000

    add clone hero controller project

--- src/components/ProjectTiles/ProjectTiles.js
+++ src/components/ProjectTiles/ProjectTiles.js
@@ -21,17 +21,6 @@ function ProjectTiles() {
 
                 <motion.div className="tile" id="tile-2" whileHover={{ scale: 1.05 }}>
                     <div className="tile-content">
-                        <h2>ElixirMix (Unity 2D)</h2>
-                        <img src={"https://github.com/Bailym/Bluefruit-GameJam-2023/blob/main/images/Mixing.png?raw=true"} alt="elixir mix game"></img>
-                    </div>
-                    <div className='tile-footer'>
-                        <a href="https://github.com/Bailym/Bluefruit-GameJam-2023" target="_blank" rel="noreferrer"><button>View on Github</button></a>
-                        <a href="https://github.com/Bailym/Bluefruit-GameJam-2023/releases/tag/v0.0.0" target="_blank" rel="noreferrer"><button>Download</button></a>
-                    </div>
-                </motion.div>
-
-                <motion.div className="tile" id="tile-3" whileHover={{ scale: 1.05 }}>
-                    <div className="tile-content">
                         <h2>Binary Bingo (React.js)</h2>
                         <img src="https://github.com/Bailym/BinaryBingo/raw/main/images/BinaryBingo%205.png" alt="Binary Bingo"></img>
 
@@ -39,7 +28,16 @@ function ProjectTiles() {
                     <div className='tile-footer'>
                         <a href="https://github.com/Bailym/BinaryBingo/" target="_blank" rel="noreferrer"><button>View on Github</button></a>
                         <a href="https://bailym.github.io/BinaryBingo/" target="_blank" rel="noreferrer"><button>View Live</button></a>
+                    </div>
+                </motion.div>
 
+                <motion.div className="tile" id="tile-3" whileHover={{ scale: 1.05 }}>
+                    <div className="tile-content">
+                        <h2>Clone Hero USB Gamepad (Arduino)</h2>
+                        <img src="https://github.com/Bailym/CloneHeroUSBController/blob/main/images/8.jpg?raw=true" alt="Clone Hero USB Gamepad (Arduino)"></img>
+                    </div>
+                    <div className='tile-footer'>
+                        <a href="https://github.com/Bailym/CloneHeroUSBController" target="_blank" rel="noreferrer"><button>View on Github</button></a>
                     </div>
                 </motion.div>
 
@@ -56,6 +54,37 @@ function ProjectTiles() {
 
                 <motion.div className="tile" id="tile-5" whileHover={{ scale: 1.05 }}>
                     <div className="tile-content">
+                        <h2>Pi Pico 1602 LCD Driver (C + Pi Pico)</h2>
+                        <img src="https://github.com/Bailym/pico-i2c-1602-lcd/blob/main/examples/still%20image.png?raw=true" alt="Pi Pico 1602 LCD Driver"></img>
+                    </div>
+                    <div className='tile-footer'>
+                        <a href="https://github.com/Bailym/pico-i2c-1602-lcd" target="_blank" rel="noreferrer"><button>View on Github</button></a>
+                    </div>
+                </motion.div>
+
+                <motion.div className="tile" id="tile-6" whileHover={{ scale: 1.05 }}>
+                    <div className="tile-content">
+                        <h2>Pi Pico KY-040 Rotary Encoder Driver (C + Pi Pico)</h2>
+                        <img src="https://github.com/Bailym/pico-ky-040/blob/main/images/USB%20serial%20out.png?raw=true" alt="Pi Pico KY-040 Rotary Encoder Driver"></img>
+                    </div>
+                    <div className='tile-footer'>
+                        <a href="https://github.com/Bailym/pico-ky-040" target="_blank" rel="noreferrer"><button>View on Github</button></a>
+                    </div>
+                </motion.div>
+
+                <motion.div className="tile" id="tile-7" whileHover={{ scale: 1.05 }}>
+                    <div className="tile-content">
+                        <h2>ElixirMix (Unity 2D)</h2>
+                        <img src={"https://github.com/Bailym/Bluefruit-GameJam-2023/blob/main/images/Mixing.png?raw=true"} alt="elixir mix game"></img>
+                    </div>
+                    <div className='tile-footer'>
+                        <a href="https://github.com/Bailym/Bluefruit-GameJam-2023" target="_blank" rel="noreferrer"><button>View on Github</button></a>
+                        <a href="https://github.com/Bailym/Bluefruit-GameJam-2023/releases/tag/v0.0.0" target="_blank" rel="noreferrer"><button>Download</button></a>
+                    </div>
+                </motion.div>
+
+                <motion.div className="tile" id="tile-8" whileHover={{ scale: 1.05 }}>
+                    <div className="tile-content">
                         <h2>TCG-DB (React.js)</h2>
                         <img src="https://github.com/Bailym/TCG-DB/blob/main/images/Base%20.png?raw=true" alt="tcg-db"></img>
                     </div>
@@ -65,7 +94,7 @@ function ProjectTiles() {
                     </div>
                 </motion.div>
 
-                <motion.div className="tile" id="tile-6" whileHover={{ scale: 1.05 }}>
+                <motion.div className="tile" id="tile-9" whileHover={{ scale: 1.05 }}>
                     <div className="tile-content">
                         <h2>GalaxyRun (Unity 2D)</h2>
                         <img src="https://github.com/Bailym/GalaxyRun/blob/main/images/Gameplay.gif?raw=true" alt="GalxyRun"></img>
@@ -73,29 +102,10 @@ function ProjectTiles() {
                     <div className='tile-footer'>
                         <a href="https://github.com/Bailym/GalaxyRun" target="_blank" rel="noreferrer"><button>View on Github</button></a>
                         <a href="https://github.com/Bailym/GalaxyRun/releases/download/v0.0.2/GalaxyRun.01-08-22.zip" target="_blank" rel="noreferrer"><button>Download</button></a>
-
                     </div>
                 </motion.div>
 
-                <motion.div className="tile" id="tile-7" whileHover={{ scale: 1.05 }}>
-                    <div className="tile-content">
-                        <h2>Pi Pico 1602 LCD Driver (C + Pi Pico)</h2>
-                        <img src="https://github.com/Bailym/pico-i2c-1602-lcd/blob/main/examples/still%20image.png?raw=true" alt="Pi Pico 1602 LCD Driver"></img>
-                    </div>
-                    <div className='tile-footer'>
-                        <a href="https://github.com/Bailym/pico-i2c-1602-lcd" target="_blank" rel="noreferrer"><button>View on Github</button></a>
-                    </div>
-                </motion.div>
-
-                <motion.div className="tile" id="tile-8" whileHover={{ scale: 1.05 }}>
-                    <div className="tile-content">
-                        <h2>Pi Pico KY-040 Rotary Encoder Driver (C + Pi Pico)</h2>
-                        <img src="https://github.com/Bailym/pico-ky-040/blob/main/images/USB%20serial%20out.png?raw=true" alt="Pi Pico KY-040 Rotary Encoder Driver"></img>
-                    </div>
-                    <div className='tile-footer'>
-                        <a href="https://github.com/Bailym/pico-ky-040" target="_blank" rel="noreferrer"><button>View on Github</button></a>
-                    </div>
-                </motion.div>
+                
             </div>
         </div>
     )
