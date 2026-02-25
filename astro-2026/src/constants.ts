84ac2336c2d4126c752ee3259406fd6bfbc12da5
Bailym <bm39@hotmail.co.uk>
2026-02-25 13:29:49 +0000

    refactor single file into components

---
+++ astro-2026/src/constants.ts
@@ -0,0 +1,8 @@
+export const name = "Baily Martin";
+
+export const links = {
+  github: "https://github.com/Bailym",
+  linkedin: "https://www.linkedin.com/in/baily-martin-16b655152/",
+  instagram: "https://www.instagram.com/bailym18/",
+  email: "BM39@hotmail.co.uk",
+};
